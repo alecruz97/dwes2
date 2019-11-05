00ee3061fc5e7ad9c4365b283b3d55444b43764f
Alejandro Cruz <alejandro.cruz@iesdonana.org>
2019-11-05 19:27:05 +0100

    Calculadora 05-11-2019

--- php prueba/calculadora/auxiliar.php
+++ php prueba/calculadora/auxiliar.php
@@ -32,19 +32,21 @@ function comprobarParametros($par, &$errores)
     return $res;
     
 }
-function comprobarValores($op1, $op2, $op, $ops, &$errores)
+function comprobarValores($args, $ops, &$errores)
 {
+    extract($args);
+
     if(!is_numeric($op1)){
-        $errores[]= 'El primer operando no es un número.';
+        $errores['op1']= 'El primer operando no es un número.';
     }
     if(!is_numeric($op2)){
-        $errores[]= 'El segundo operando no es un número.';
+        $errores['op2']= 'El segundo operando no es un número.';
     }
     if(!in_array($op, $ops)){
-        $errores[]= 'El operador no es correcto.';
+        $errores['op']= 'El operador no es correcto.';
     }
     if($op == 2 && $op == '/'){
-        $errores[] = 'No se puede dividir por 0.';
+        $errores['op'] = 'No se puede dividir por 0.';
     }
     comprobarErrores($errores);
 }
@@ -54,10 +56,19 @@ function comprobarValores($op1, $op2, $op, $ops, &$errores)
  * @param string $m
  * @return void
  */
-function mensajeError(string $m): void
-{ ?>
-    <div class="error"><?= $m ?></div><?php
+function mensajeError($campo, $errores)
+{
+    if(isset($errores[$campo])){
+        return <<<EOT
+        <div class="invalid-feedback">
+            {$errores[$campo]}
+        </div>
+        EOT;
+    }else{
+        return '';
+    }
 }
+
 function comprobarErrores($errores)
 {
     if (empty($_GET) || !empty($errores)) {
@@ -70,31 +81,46 @@ function selected ($op, $o)
     return $op == $o ? 'selected' : '';
 }
 
-function dibujarFormulario($op1, $op2, $op, $ops)
-{ 
-    ?>
-    <form action="" method="get">
-        <label for="op1">Primer operando:</label>
-        <input type="text" id="op1" name="op1" value="<?= $op1 ?>">
-        <br>
-        <label for="op2">Segundo operando:</label>
-        <input type="text" id="op2" name="op2" value="<?= $op2 ?>">
-        <br>
-        <label for="op">Operación:</label>
-        <select name="op">
-            <?php foreach ($ops as $o): ?>
-            <option value="<?= $o?>" <?= selected($op, $o) ?>><?= $o?></option>
-            <?php endforeach ?>
-        </select>
-        <br>
-        <button type="submit">Calcular</button>
-    </form>
-    <?php
+function valido($campo, $errores)
+{
+    if(isset($errores[$campo])){
+        return 'is-invalid';
+    } else if(!empty($_GET)){
+        return 'is-valid';
+    } else{
+        return '';
+    }
 }
 
-function mostrarErrores($errores)
-{
-    foreach ($errores as $error) {
-        mensajeError($error);
-    }
+function dibujarFormulario($args, $ops, $errores)
+{ 
+    extract($args);
+    ?>
+    <form action="" method="get">
+        <div class="form-group">
+            <label for="op1">Primer operando:</label>
+            <input type="text" class="form-control <?= valido('op1', $errores) ?>"
+            id="op1" name="op1" value="<?= $op1 ?>">
+            <?= mensajeError('op1', $errores) ?>
+        </div>
+        <div class="form-group">
+            <label for="op2">Segundo operando:</label>
+            <input type="text" id="op2" class="form-control <?= valido('op2', $errores) ?>"
+            name="op2" value="<?= $op2 ?>">
+            <?= mensajeError('op2', $errores) ?>
+        </div>
+        <div class="form-group">
+                <label for="op">Operación:</label>
+            <select name="op" class="form-control <?= valido('op', $errores) ?>">
+                <?php foreach ($ops as $o): ?>
+                <option value="<?= $o?>" <?= selected($op, $o) ?>><?= $o?></option>
+                <?php endforeach ?>
+            </select>
+            <?= mensajeError('op', $errores) ?>
+        </div>
+        
+        <button type="submit" class="btn btn-primary">Calcular</button>
+        
+    </form>
+    <?php
 }
--- php prueba/calculadora/index.php
+++ php prueba/calculadora/index.php
@@ -1,32 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
     <title>Calculadora</title>
-</head>
-<body>
-    <?php
-    require __DIR__ . '/auxiliar.php';
-    const OPS = ['+', '-', '*', '/'];
-    const PAR = ['op1' => "", 'op2' => "", 'op' => '+'];
+  </head>
+  <body>
+      <div class="container">
+          <div class="row">
+                <div class="col">
+                    <?php
 
-    $errores = [];
-    
-    
-    try {
-        extract(comprobarParametros(PAR, $errores));
-        comprobarErrores($errores);
-        comprobarValores($op1, $op2, $op, OPS, $errores);
-        calcular($op1, $op2, $op);
-    } catch (Exception $e) {
-    }
+                    require __DIR__ . '/auxiliar.php';
+                    const OPS = ['+', '-', '*', '/'];
+                    const PAR = ['op1' => "", 'op2' => "", 'op' => '+'];
 
-    dibujarFormulario($op1, $op2, $op, OPS);
+                    $errores = [];
+            
+                    try {
+                        extract(comprobarParametros(PAR, $errores));
+                        comprobarErrores($errores);
+                        comprobarValores($op1, $op2, $op, OPS, $errores);
+                        calcular($op1, $op2, $op);
+                    } catch (Exception $e) {
+                    }
+            
+                    dibujarFormulario($op1, $op2, $op, OPS, $errores);
+            
+                    ?>
+                </div>
+            </div>
+        </div>
 
-    mostrarErrores($errores);
-    
-    ?>
-</body>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
 </html>
